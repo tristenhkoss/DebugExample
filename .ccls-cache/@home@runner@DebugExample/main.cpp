6307e672297f0af731e2a84718016fae1f0531ae
tristenhkoss <142366109+tristenhkoss@users.noreply.github.com>
2023-08-24 15:06:58 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@DebugExample/main.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+   const int NUM_ELEMENTS = 8;        // Number of elements
+   vector<int> revVctr(NUM_ELEMENTS); // User values
+   unsigned int i;                    // Loop index
+   int top, bottom;
+       
+   cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
+   for (i = 0; i < revVctr.size(); ++i) {
+      cout << "Value: ";
+      cin >> revVctr.at(i);
+   }
+   
+   // Reverse
+   for (i = 0; i < revVctr.size() / 2; ++i) {
+      bottom = revVctr.at(i);
+      revVctr.at(i) = revVctr.at(revVctr.size() - i - 1); // Swap
+      revVctr.at(revVctr.size() - i - 1) = bottom;
+
+   }
+   
+   // Print values
+   cout << endl << "New values: ";
+   for (i = 0; i < revVctr.size(); ++i) {
+      cout << " " << revVctr.at(i);
+   }
+   cout << endl;
+   
+   return 0;
+}
