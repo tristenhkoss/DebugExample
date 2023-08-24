6a79d55e585c4c215fe101c8272869aa438a827e
tristenhkoss <142366109+tristenhkoss@users.noreply.github.com>
2023-08-24 15:18:41 +0000

    Start of code

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
    const int NUM_ELEMENTS = 8;        // Number of elements
    vector<int> revVctr(NUM_ELEMENTS); // User values
    unsigned int i;                    // Loop index
-   int top, bottom;
+   int bottom;
        
    cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
    for (i = 0; i < revVctr.size(); ++i) {
